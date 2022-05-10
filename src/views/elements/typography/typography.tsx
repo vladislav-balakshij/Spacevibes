c83847068d9f4d064826888464ad5618dff40923
Vladyslav Balakshii <vladislav.balakshij@gmail.com>
2022-05-10 22:00:30 +0300

    add styles

--- src/views/elements/typography/typography.tsx
+++ src/views/elements/typography/typography.tsx
@@ -4,7 +4,7 @@ function Heading({level, children}) {
 
   const heading = useCallback(() => {
     switch (level) {
-      case 1: return <h1 className={styles.h1}>{children}</h1>;
+      case 1: return <h1>{children}</h1>;
       case 2: return <h2>{children}</h2>;
       case 3: return <h3>{children}</h3>;
       case 4: return <h4>{children}</h4>;
