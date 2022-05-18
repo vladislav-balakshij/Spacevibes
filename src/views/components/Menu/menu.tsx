7ee7696c6ab4e7ff0a13121d8c42fd13084cfede
Vladyslav Balakshii <vladislav.balakshij@gmail.com>
2022-05-18 19:39:02 +0300

    menu modifications

--- src/views/components/Menu/menu.tsx
+++ src/views/components/Menu/menu.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 import * as styles from './menu.module.scss';
+
+import IconClose from '../../../assets/icons/iconClose.svg';
+import IconDribble from '../../../assets/icons/iconDribble.svg';
+import IconFacebook from '../../../assets/icons/iconFacebook.svg';
+import IconInstagram from '../../../assets/icons/iconInstagram.svg';
+import IconLinkedIn from '../../../assets/icons/iconLinkedIn.svg';
+import IconTwitter from '../../../assets/icons/iconTwitter.svg';
+
 import { GlobalContext } from '../../layouts/mainLayout';
 
 const Menu = () => {
@@ -8,10 +16,41 @@ const Menu = () => {
 
   return (
     <div className={styles.overlay} onClick={() => {
-      setState({...state, showMenu : !state.showMenu})
+      setState({ ...state, showMenu: !state.showMenu })
     }}>
       <div className={styles.menu}>
-        menu
+        <button>
+          <IconClose />
+        </button>
+        <div>
+          <a href="#">
+            <IconDribble />
+          </a>
+          <a href="#">
+            <IconFacebook />
+          </a>
+          <a href="#">
+            <IconInstagram />
+          </a>
+          <a href="#">
+            <IconLinkedIn />
+          </a>
+          <a href="#">
+            <IconTwitter />
+          </a>
+        </div>
+        <ul className={styles.list}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Project</a></li>
+          <li><a href="#">Approach</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Insights</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <div>
+          Book a free consultation call
+          Schedule a call
+        </div>
       </div>
     </div>
   )
